4f2881b50a024196ecee6455f8679f064c8fcb11
Himanshu <24f2003563@ds.study.iitm.ac.in>
2026-07-06 18:25:14 +0530

    Update metrics-api.py

--- metrics-api.py
+++ metrics-api.py
@@ -1,69 +1,40 @@
-from fastapi import FastAPI, Query
-from fastapi.middleware.cors import CORSMiddleware
-
-import os
-import yaml
-from dotenv import load_dotenv
+from fastapi import FastAPI
+import redis
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+r = redis.Redis(
+    host="redis",
+    port=6379,
+    decode_responses=True
 )
 
-load_dotenv()
-
-os.environ["APP_PORT"] = "8019"
-os.environ["APP_DEBUG"] = "false"
-
-
-def to_bool(value):
-    return str(value).lower() in ["true", "1", "yes", "on"]
-
-
-@app.get("/effective-config")
-def effective_config(set: list[str] = Query(default=[])):
-    config = {
-        "port": 8000,
-        "workers": 1,
-        "debug": False,
-        "log_level": "info",
-        "api_key": "default-secret-000",
+@app.post("/hit/{key}")
+def hit(key: str):
+    count = r.incr(key)
+    return {
+        "key": key,
+        "count": count
     }
 
-    with open("config.development.yaml") as f:
-        config.update(yaml.safe_load(f))
+@app.get("/count/{key}")
+def count(key: str):
+    value = r.get(key)
 
-    if os.getenv("APP_PORT"):
-        config["port"] = int(os.getenv("APP_PORT"))
+    if value is None:
+        value = 0
+    else:
+        value = int(value)
 
-    if os.getenv("NUM_WORKERS"):
-        config["workers"] = int(os.getenv("NUM_WORKERS"))
+    return {
+        "key": key,
+        "count": value
+    }
 
-    if os.getenv("APP_LOG_LEVEL"):
-        config["log_level"] = os.getenv("APP_LOG_LEVEL")
-
-    if os.getenv("APP_PORT"):
-        config["port"] = int(os.getenv("APP_PORT"))
-
-    if os.getenv("APP_DEBUG"):
-        config["debug"] = to_bool(os.getenv("APP_DEBUG"))
-
-    for item in set:
-        key, value = item.split("=", 1)
-
-        if key in ["port", "workers"]:
-            config[key] = int(value)
-
-        elif key == "debug":
-            config[key] = to_bool(value)
-
-        else:
-            config[key] = value
-
-    config["api_key"] = "****"
-
-    return config
+@app.get("/healthz")
+def health():
+    if r.ping():
+        return {
+            "status": "ok",
+            "redis": "up"
+        }
